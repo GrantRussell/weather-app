a9b72eed3323399f914053a26217f27fbaa270fd
GrantRussell <grant.s.russ@gmail.com>
2017-09-22 13:52:23 -0600

    Fianl push up of weather app, starting on promise.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const request = require('request');
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs
   .options({
@@ -20,6 +20,14 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   if(errorMessage){
     console.log(errorMessage);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log(results.address);
+    weather.getWeather(results.lat, results.lng, (errorMessage, weatherResults) => {
+      if(errorMessage){
+        console.log(errorMessage);
+      } else{
+        console.log('Current Temperature: ' + JSON.stringify(weatherResults.temperature, undefined, 2));
+        console.log('Apparent Temperature: ' + JSON.stringify(weatherResults.apparentTemperature, undefined, 2));
+      }
+    });
   }
 });
